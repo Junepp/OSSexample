baa2a3b33ba309307794046892e54d47603d7b52
Junepp <82352056+Junepp@users.noreply.github.com>
2021-11-16 20:57:14 +0900

    Create account.c

---
+++ account.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  int account[3] = {9230, 27000, 15000);
+  int i=0;
+  for(i; i<3;i++){
+    printf("%d\n", account[i]);
+  }
+  printf("version 1.0.0\n");
+  return 0;
+}
